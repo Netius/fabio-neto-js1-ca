739108016dd26816b85a388b78b10243e8e0a0ed
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2020-05-26 11:13:52 +0200

    Changed for loop to forEach

--- js/about.js
+++ js/about.js
@@ -1,10 +1,11 @@
 function changeWords() {
-    const paragraph = document.querySelectorAll(".about p:not(:last-child)");
-    for (let i = 0; i < paragraph.length; i++) {
-        let changedParagraph = "";
-        changedParagraph = paragraph[i].innerText.replace(/the /g, "banana ").replace(/The /g, "Banana ");
-        paragraph[i].innerHTML = changedParagraph;
-    }
+    const paragraph = document.querySelectorAll(".about p:not(:last-child)"); //Selects all P tags but not the last one (its a link)
+
+    paragraph.forEach((phrase) => {
+        phrase.innerText = phrase.innerText.replace(/the /g, "banana ")
+            .replace(/The /g, "Banana ")
+    });
+
 }
 
 setTimeout(changeWords, 3000);
